a22d4c2a3b779874262c440228fa660189ee1c28
Gangula007415 <gangulagajjala@gmail.com>
2026-06-18 16:20:24 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Date
+from database import Base
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    patient_id = Column(Integer, primary_key=True, index=True)
+    patient_name = Column(String(100), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(20), nullable=False)
+    disease = Column(String(100), nullable=False)
+    doctor_name = Column(String(100), nullable=False)
+    admission_date = Column(Date, nullable=False)
+    phone_number = Column(String(15), nullable=False)
